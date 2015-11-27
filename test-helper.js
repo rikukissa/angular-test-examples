ba8318d48a8298a6472a94b900e4470c0fffdf14
Riku Rouvila <riku.rouvila@gmail.com>
2015-11-27 10:17:24 +0200

    add Node from window to global variables

--- test-helper.js
+++ test-helper.js
@@ -4,7 +4,10 @@ var jsdom = require('jsdom').jsdom;
 
 var document = global.document = jsdom('<html><head></head><body></body></html>');
 var window = global.window = document.defaultView;
+
+
 global.navigator = window.navigator = {};
+global.Node = window.Node ;
 
 /*
  * angular-mocks
